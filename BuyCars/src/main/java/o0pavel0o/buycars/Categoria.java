a169ef4d7dd0456376c2b0ac156c6fef352623cd
orluzuriaga <renatoluzuriaga@gmail.com>
2017-03-06 00:24:37 +0100

    subo entidad categoria

---
+++ BuyCars/src/main/java/o0pavel0o/buycars/Categoria.java
@@ -0,0 +1,54 @@
+package o0pavel0o.buycars;
+
+import javax.persistence.*;
+
+@Entity
+public class Categoria {
+
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+    private String tipoCategoria;
+   
+	 
+    
+	  //Constructor necesario para la carga de la BBDD
+	    protected Categoria(){}
+
+
+	public Categoria(String tipoCategoria) {
+	
+		this.tipoCategoria = tipoCategoria;
+		
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getTipoCategoria() {
+		return tipoCategoria;
+	}
+
+
+	public void setTipoCategoria(String tipoCategoria) {
+		this.tipoCategoria = tipoCategoria;
+	}
+
+
+	
+	
+	    
+	  
+	
+	    
+}
---
+++ BuyCars/src/main/java/o0pavel0o/buycars/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package o0pavel0o.buycars;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	
+	
+	Categoria findByTipoCategoria(String cat);
+
+}
