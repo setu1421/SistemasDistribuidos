dd7fb91208d2264e0e506913b65095fd7e704cf6
orluzuriaga <renatoluzuriaga@gmail.com>
2017-03-06 00:53:41 +0100

    subo repositorio coche

--- BuyCars/src/main/java/o0pavel0o/buycars/Coche.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/Coche.java
@@ -1,5 +1,221 @@
 package o0pavel0o.buycars;
 
+
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class Coche {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	    
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String combustible;
+    private String color;
+    private String potencia;
+    private double kilometros;
+    private int plazas;
+    private double precio;
+    
+   // private List<String> equipamientoExtra;
+    
+    
+  //Constructor necesario para la carga de la BBDD
+    protected Coche(){}
+
+    
+
+
+	public Coche(String matricula, String marca, String modelo, String combustible, String color, String potencia,
+			double kilometros, int plazas, double precio) {
+		
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.combustible = combustible;
+		this.color = color;
+		this.potencia = potencia;
+		this.kilometros = kilometros;
+		this.plazas = plazas;
+		this.precio = precio;
+		
+	}
+
+
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+
+	public String getCombustible() {
+		return combustible;
+	}
+
+
+
+
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
+	}
+
+
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+
+
+	public String getPotencia() {
+		return potencia;
+	}
+
+
+
+
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
+
+
+
+
+	public Double getKilometros() {
+		return kilometros;
+	}
+
+
+
+
+	public void setKilometros(float kilometros) {
+		this.kilometros = kilometros;
+	}
+
+
+
+
+	public int getPlazas() {
+		return plazas;
+	}
+
+
+
+
+	public void setPlazas(int plazas) {
+		this.plazas = plazas;
+	}
+
+
+
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+
+
+	/*public List<String> getEquipamientoExtra() {
+		return equipamientoExtra;
+	}
+
+
+
+
+	public void setEquipamientoExtra(List<String> equipamientoExtra) {
+		this.equipamientoExtra = equipamientoExtra;
+	}*/
+
+
+
+
+
+    
+    
+	
+	
+	
+    
+	
 }
+
