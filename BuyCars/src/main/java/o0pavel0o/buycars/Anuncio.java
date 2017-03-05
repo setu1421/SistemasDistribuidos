b1f62502c1f3084280dbf18d7f10f82179e283d4
orluzuriaga <renatoluzuriaga@gmail.com>
2017-03-05 13:53:17 +0100

    Comentarios en constructor

--- BuyCars/src/main/java/o0pavel0o/buycars/Anuncio.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/Anuncio.java
@@ -1,62 +1,15 @@
-<<<<<<< HEAD
 package o0pavel0o.buycars;
 
 public class Anuncio {
 
-	private String nombre;
-	private String asunto;
-	private String comen;
-    
-    
-	public Anuncio() {
-
-	}
-
-	public Anuncio(String nombre, String asunto, String comentario) {
-		super();
-		this.nombre = nombre;
-		this.asunto = asunto;
-		this.comentario = comentario;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getAsunto() {
-		return asunto;
-	}
-
-	public void setAsunto(String asunto) {
-		this.asunto = asunto;
-	}
-
-	public String getComentario() {
-		return comentario;
-	}
-
-	public void setComentario(String comentario) {
-		this.comentario = comentario;
-	}
-
-	@Override
-	public String toString() {
-		return "Anuncio [nombre=" + nombre + ", asunto=" + asunto + ", comentario=" + comentario + "]";
-	}
-
-}
-=======
-package o0pavel0o.buycars;
-
-public class Anuncio {
-
+	
 	private String nombre;
 	private String asunto;
 	private String comentario;
+    
+
+	
+	//Constructor  
 	public Anuncio() {
 
 	}
@@ -98,4 +51,3 @@ public class Anuncio {
 	}
 
 }
->>>>>>> origin/master
