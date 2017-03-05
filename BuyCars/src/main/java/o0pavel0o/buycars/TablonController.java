6a0b609eb5a793ff0c2002363d409dcd801c8b5f
Pablo <padelarco@gmail.com>
2017-03-05 23:38:56 +0100

    subo controller que funciona

--- BuyCars/src/main/java/o0pavel0o/buycars/TablonController.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/TablonController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import repositorios.AnunciosRepository;
-
 @Controller
 public class TablonController {
 
