6a60c08b9879686f62bb60772fb188ca6dfa8538
Pablo <padelarco@gmail.com>
2017-03-18 21:26:07 +0100

    abro ver anuncio a la parte publica

--- BuyCars/src/main/java/o0pavel0o/buycars/SecurityConfiguration.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/SecurityConfiguration.java
@@ -16,6 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/login").permitAll();
         http.authorizeRequests().antMatchers("/loginerror").permitAll();
         http.authorizeRequests().antMatchers("/logout").permitAll();
+        http.authorizeRequests().antMatchers("/anuncio/{id}").permitAll();
 
         // Private pages (all other pages)
         http.authorizeRequests().anyRequest().authenticated();
