beb3a68daa4b046ecfdc01400b19c39e13232aa0
Pablo <padelarco@gmail.com>
2017-03-18 21:40:52 +0100

    meto los tokens de los formularios

--- BuyCars/src/main/java/o0pavel0o/buycars/SecurityConfiguration.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/SecurityConfiguration.java
@@ -20,6 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/comprar/{id}").permitAll();
         http.authorizeRequests().antMatchers("/vender/{id}").permitAll();
         http.authorizeRequests().antMatchers("/nuevoUsuario.html").permitAll();
+        http.authorizeRequests().antMatchers("/usuario/nuevo").permitAll();
         
 
         // Private pages (all other pages)
