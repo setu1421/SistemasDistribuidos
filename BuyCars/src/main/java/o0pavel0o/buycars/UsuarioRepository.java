ed367b360ba8d01fd81cf10c885fbc0754e0b476
orluzuriaga <renatoluzuriaga@gmail.com>
2017-03-19 20:10:43 +0100

    prueba

--- BuyCars/src/main/java/o0pavel0o/buycars/UsuarioRepository.java
+++ BuyCars/src/main/java/o0pavel0o/buycars/UsuarioRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 
-	
+	Usuario findByNombre(String nombre);
 
 }
